e5b701e2f53d6b3aa8d564e1e167d05502da6fad
adicasa <juarmando.diaz36@gmail.com>
2020-09-03 16:13:13 -0400

    Envio cambio de dos programas principales a uno

--- paquetes/transfor.go
+++ paquetes/transfor.go
@@ -1,11 +1,18 @@
 package main
 
+//go-paquetes.git
 import (
 	"fmt"
 	"paquetes/numeros"
+	"paquetes/saludo"
 )
 
 func main() {
 	fmt.Println(numeros.Convertir(16))
+	//fmt.Println(numeros.IsPrime(19))
+	//fmt.Println(greeting.WelcomeText)
+	//fmt.Println(strings.Reverse("adicasa"))
+	//fmt.Println(str.Count("Go es Amigable y agradable. Me gusta trabajar en Go", "Go"))
 
+	fmt.Println(saludo.Holamundo() + " de adicasa")
 }
